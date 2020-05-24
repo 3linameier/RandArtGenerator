7be33bd19aaed3e198b71a3629ba958b06065b03
Elina Meier <56025134+3linameier@users.noreply.github.com>
2020-05-24 22:49:21 +0300

    kõik failid

---
+++ src/Projekt/TühiSisendErind.java
@@ -0,0 +1,16 @@
+/*************************************************
+ * Random Art Generator
+ *
+ * Autorid: Elina Meier, Kristjan Pühvel
+ *
+ * Tartu Ülikool
+ * kevadsemester 2020
+ ************************************************/
+
+package Projekt;
+
+public class TühiSisendErind extends Exception {
+    public TühiSisendErind(String message) {
+        super(message);
+    }
+}
